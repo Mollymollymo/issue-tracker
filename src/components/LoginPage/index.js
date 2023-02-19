fd02c393f5ddbbe268618bf823de3e76377e46d9
Molly <erdy521@gmail.com>
2023-02-19 17:32:08 +0800

    feat: add login page

---
+++ src/components/LoginPage/index.js
@@ -0,0 +1,11 @@
+const LoginPage = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center ">
+      <button className="bg-black text-white w-64 h-12 rounded-xl">
+        Sign in with GitHub
+      </button>
+    </div>
+  );
+};
+
+export default LoginPage;
