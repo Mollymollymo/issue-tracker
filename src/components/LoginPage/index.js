d4ee368bd08fc12ade8c773c78aa68fd1e3ccf43
Molly <erdy521@gmail.com>
2023-02-19 17:51:24 +0800

    feat: add github oauth login

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,7 +1,15 @@
 const LoginPage = () => {
+  const CLIENT_ID = "c3c17e5a1f1107857a99";
+  const handleLogin = () => {
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=http://localhost:3000/`;
+  };
+
   return (
     <div className="w-screen h-screen flex justify-center items-center ">
-      <button className="bg-black text-white w-64 h-12 rounded-xl">
+      <button
+        className="bg-black text-white w-64 h-12 rounded-xl"
+        onClick={handleLogin}
+      >
         Sign in with GitHub
       </button>
     </div>
