49140260fd80bd5d17f06afc718cc197707b6604
Molly <erdy521@gmail.com>
2023-03-20 16:22:49 +0800

    feat:create DoneIssues IU

---
+++ src/components/DoneIssues/index.js
@@ -0,0 +1,13 @@
+import Issuses from "../Issuses";
+
+const DoneIssues = () => {
+  return (
+    <div className="w-screen h-screen flex-col ">
+      <Issuses title="title7" body="body7" />
+      <Issuses title="title8" body="body8" />
+      <Issuses title="title9" body="body9" />
+    </div>
+  );
+};
+
+export default DoneIssues;
